e3ef09e3cf8071855656a99a79ac63acbad06840
Akriti <vermaakriti700@gmail.com>
2022-08-09 09:46:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    const purple="red";
+    const [bg, setBg]= useState(purple);
+    const [name,setName]=useState("Click Me");
+  const bgChange=()=>{
+    let newbg="#34495e";
+    setBg(newbg);
+    setName('Ouch!! 😱');
+    
+
+  }
+  
+  let co="#008080";
+  const bgBack = () =>{
+    setBg(co);
+    setName("omg!! 😠");
+  }
+  return(
+       <>
+        <div style={{backgroundColor: bg}}>
+          <button onClick={bgChange} onDoubleClick={bgBack}> {name} </button>
+        </div>
+       </>
+  )
 }
+//cd greetings
+  //npm start
 
 export default App;
